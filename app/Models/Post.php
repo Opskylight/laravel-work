b0a069d3e63db3a611557a559eb1cf3afc4371a9
John Doe <johndoe@example.com>
2020-12-15 16:14:48 +0800

    ok

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $fillable = [
         'content'
     ];
@@ -19,7 +21,7 @@ class Post extends Model
 public function subject()
 {
     return $this->belongsTo(Subject::class);
-} 
+}
 
 public function user()
 {
