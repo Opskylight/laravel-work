9338d07dd103b1e981a303ceb06d3d5b5d2e9a6a
John Doe <johndoe@example.com>
2020-12-15 16:10:50 +0800

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'content'
+    ];
+
+/**
+ * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+ */
+
+public function subject()
+{
+    return $this->belongsTo(Subject::class);
+} 
+
+public function user()
+{
+    return $this->belongsTo(User::class);
+}
+
+}
